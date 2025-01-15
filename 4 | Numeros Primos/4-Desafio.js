91ada3e707c631292d59a453be115722b9430305
AnderCMD <andercmd@outlook.com>
2025-01-14 19:17:56 -0600

    Agregar solución para comprobar números primos e imprimir los primos entre 1 y 100

---
+++ 4 | Numeros Primos/4-Desafio.js
@@ -0,0 +1,12 @@
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
+
+// ? Solución
+
+for (i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    } 
+}
